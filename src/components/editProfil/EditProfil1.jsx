2d981edf0088f55ea8e15d5d5e421e91a297b535
Azhari <azharirambe2003@gmail.com>
2024-12-16 20:03:57 +0800

    Update Profile ke 2

--- src/components/editProfil/EditProfil1.jsx
+++ src/components/editProfil/EditProfil1.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Headerprofil from "../headerNavBreadcrumbs/HeaderWebProfile";
+import Headerprofil from "../headerNavBreadcrumbs/HeaderProfil";
 import { useNavigate } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {

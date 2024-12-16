0640e2d948ccfc066cd4b381299ce887d7f87aa2
mcahyo <11211056@student.itk.ac.id>
2024-12-17 06:21:21 +0800

    update about page

--- src/components/website/Footer/About.jsx
+++ src/components/website/Footer/About.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
-import Logo from '../../../assets/logo/logoLogin.png'
-import Rectangle from '../../../assets/assetabout/Rectangle 645.png'
-import Hand from '../../../assets/assetabout/Female hand holding iPhone 14 Pro.png'
-import Screen from '../../../assets/assetabout/iPhone SE - 2.png'
-import Frame1 from '../../../assets/assetabout/Frame 1.png' 
-import Frame2 from '../../../assets/assetabout/Frame 2.png'
-import Frame3 from '../../../assets/assetabout/Frame 3.png'
-import Frame4 from '../../../assets/assetabout/Frame 4.png'
+import Logo from '../../../assets/logo/logoLogin.png';
+import Rectangle from '../../../assets/assetabout/Rectangle 645.png';
+import Hand from '../../../assets/assetabout/Female hand holding iPhone 14 Pro.png';
+import Screen from '../../../assets/assetabout/iPhone SE - 2.png';
+import Frame1 from '../../../assets/assetabout/Frame 1.png'; 
+import Frame2 from '../../../assets/assetabout/Frame 2.png';
+import Frame3 from '../../../assets/assetabout/Frame 3.png';
+import Frame4 from '../../../assets/assetabout/Frame 4.png';
+import Photo1 from '../../../assets/assetabout/Photo 1.png';
+import Photo2 from '../../../assets/assetabout/Photo 2.png';
 
 // Main LandingPage component
 const About = () => {
+  // Create a reference to the reviews section
+  const reviewsRef = useRef(null);
+
+  // Function to handle scrolling to the reviews section
+  const scrollToReviews = () => {
+    if (reviewsRef.current) {
+      reviewsRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+
   return (
     <>
       <link
@@ -19,53 +32,55 @@ const About = () => {
         rel="stylesheet"
       />
 
-<header className="bg-[#FAFAFAFA] shadow-md py-2 px-4">
-  <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between mb-5">
-    {/* Logo and Menu Combined */}
-    <div className="flex flex-col items-center w-full sm:flex-row sm:justify-between"> {/* Center logo and adjust layout for smaller screens */}
-      {/* Logo */}
-      <div className="flex items-center justify-center sm:justify-start w-full sm:w-auto mb-2 sm:mb-0">
-        <img
-          src={Logo}
-          alt="PixelStore Logo"
-          className="h-20 w-20 sm:h-20 sm:w-20 md:h-20 md:w-20 lg:h-20 lg:w-20 xl:h-20 xl:w-20" // Uniform size for md and larger screens
-        />
-      </div>
-      {/* Menu and Button */}
-      <div className="flex-grow flex justify-center sm:justify-end sm:mt-0">
-      <nav className="flex items-center space-x-4 sm:space-x-10 mx-[-10px] ml-4 sm:ml-6 md:ml-8 lg:ml-10 xl:ml-12">
-      <ul className="flex space-x-4 text-black font-poppins text-xs">
-            <li>
-              <Link to="/" className="hover:text-gray-500">Home</Link>
-            </li>
-            <li>
-              <Link to="/assets" className="hover:text-gray-500">Assets</Link>
-            </li>
-            <li>
-              <Link to="/testimonials" className="hover:text-gray-500">Testimonials</Link>
-            </li>
-          </ul>
+      <header className="bg-[#FAFAFAFA] shadow-md py-2 px-4">
+        <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between mb-5">
+          {/* Logo and Menu Combined */}
+          <div className="flex flex-col items-center w-full sm:flex-row sm:justify-between"> {/* Center logo and adjust layout for smaller screens */}
+            {/* Logo */}
+            <div className="flex items-center justify-center sm:justify-start w-full sm:w-auto mb-2 sm:mb-0">
+              <img
+                src={Logo}
+                alt="PixelStore Logo"
+                className="h-20 w-20 sm:h-20 sm:w-20 md:h-20 md:w-20 lg:h-20 lg:w-20 xl:h-20 xl:w-20" // Uniform size for md and larger screens
+              />
+            </div>
+            {/* Menu and Button */}
+            <div className="flex-grow flex justify-center sm:justify-end sm:mt-0">
+              <nav className="flex items-center space-x-4 sm:space-x-10 mx-[-10px] ml-4 sm:ml-6 md:ml-8 lg:ml-10 xl:ml-12">
+                <ul className="flex space-x-4 text-black font-poppins text-xs">
+                  <li>
+                    <Link to="/" className="hover:text-gray-500">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/asset-gratis" className="hover:text-gray-500">Assets</Link>
+                  </li>
+                  <li>
+                    <button 
+                      onClick={scrollToReviews}
+                      className="hover:text-gray-500">Testimonials</button>
+                  </li>
+                </ul>
 
-          {/* Get the App Button */}
-          <a
-            href="/"
-            className="bg-[#5282ED] text-xs text-white font-poppins py-1 px-1 sm:px-8 rounded ml-0 sm:ml-5" // Use responsive padding and margin
-          >
-            Get The App Web →
-          </a>
-        </nav>
-      </div>
-    </div>
-  </div>
-</header>
-      <Body />
+                {/* Get the App Button */}
+                <a
+                  href="/"
+                  className="bg-[#5282ED] text-xs text-white font-poppins py-1 px-1 sm:px-8 rounded ml-0 sm:ml-5"
+                >
+                  Go  The  Web →
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      <Body reviewsRef={reviewsRef} />
       <Footer />
     </>
   );
 };
 
 // Body component containing main content
-const Body = () => {
+const Body = ({ reviewsRef }) => {
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen bg-[#FAFAFAFA] relative px-2 sm:px-4 md:px-8 lg:px-12 xl:px-16">
       {/* Main Content */}
@@ -78,9 +93,9 @@ const Body = () => {
           <p className="text-sm sm:text-base md:text-lg text-gray-500 ml-0">
             Empower Your Assets and Expand Your Reach!
           </p>
-          <button className="px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 text-white bg-bl ue-500 rounded hover:bg-blue-600 flex items-center ml-0">
+          <button className="px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 text-white bg-blue-500 rounded hover:bg-blue-600 flex items-center ml-0">
             <Link to="/login" className="flex items-center">Login Now → <span className="fas fa-arrow-right ml-2"></span></Link>
-            </button>
+          </button>
         </div>
 
         {/* Right Section */}
@@ -140,64 +155,64 @@ const Body = () => {
         </div>
       </div>
 
-{/* Features Section */}
-<section className="text-center py-16">
-  {/* Flex container for left and right sections */}
-  <div className="flex flex-col md:flex-row justify-between items-start w-screen">
-    
-    {/* Left Section */}
-    <div className="mb-5 pt-28 mt-12 text-left w-full md:w-[45%] px-6 md:px-10 md:ml-[-10px]"> 
-      <h2 className="text-3xl font-bold">
-        Discover the Benefits <br /> of <span className="text-[#5282ED]">Pixel Store</span>
-      </h2>
-      <p className="text-gray-600 mt-4">
-        Empower Your Assets and Expand Your Reach!
-      </p>
-    </div>
+      {/* Features Section */}
+      <section className="text-center py-16">
+        {/* Flex container for left and right sections */}
+        <div className="flex flex-col md:flex-row justify-between items-start w-screen">
+          
+          {/* Left Section */}
+          <div className="mb-5 pt-28 mt-12 text-left w-full md:w-[45%] px-6 md:px-10 md:ml-[-10px]"> 
+            <h2 className="text-3xl font-bold">
+              Discover the Benefits <br /> of <span className="text-[#5282ED]">Pixel Store</span>
+            </h2>
+            <p className="text-gray-600 mt-4">
+              Empower Your Assets and Expand Your Reach!
+            </p>
+          </div>
 
-    {/* Right Section */}
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:w-[55%] px-20">
-      <FeatureBox
-        iconSrc={Frame1}
-        title="High-Quality Asset"
-        description="Pixel Store offers a variety of visual assets, from photos and illustrations to 3D design elements."
-        bgColor="bg-blue-500 text-white"
-        roundedClass="rounded-tl"
-        borderRadius={30}
-        size={250}
-      />
-      <FeatureBox
-        iconSrc={Frame2}
-        title="Affordable Licensing"
-        description="Select from a range of budget-friendly licensing options for personal or commercial use."
-        bgColor="bg-white text-black"
-        roundedClass="rounded-tr"
-        borderRadius={30}
-        size={250}
-      />
-      <FeatureBox
-        iconSrc={Frame3}
-        title="User-Friendly Interface"
-        description="Pixel Store makes it easy to browse and download assets, ensuring a smooth user experience."
-        bgColor="bg-white text-black"
-        roundedClass="rounded-bl"
-        borderRadius={30}
-        size={250}
-      />
-      <FeatureBox
-        iconSrc={Frame4}
-        title="Collaborative Community"
-        description="Share your work and gain valuable feedback to grow as an artist or creator."
-        bgColor="bg-white text-black"
-        roundedClass="rounded-br"
-        borderRadius={30}
-        size={250}
-      />
-    </div>
-  </div>
-</section>
+          {/* Right Section */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:w-[55%] px-20">
+            <FeatureBox
+              iconSrc={Frame1}
+              title="High-Quality Asset"
+              description="Pixel Store offers a variety of visual assets, from photos and illustrations to 3D design elements."
+              bgColor="bg-blue-500 text-white"
+              roundedClass="rounded-tl"
+              borderRadius={30}
+              size={250}
+            />
+            <FeatureBox
+              iconSrc={Frame2}
+              title="Affordable Licensing"
+              description="Select from a range of budget-friendly licensing options for personal or commercial use."
+              bgColor="bg-white text-black"
+              roundedClass="rounded-tr"
+              borderRadius={30}
+              size={250}
+            />
+            <FeatureBox
+              iconSrc={Frame3}
+              title="User-Friendly Interface"
+              description="Pixel Store makes it easy to browse and download assets, ensuring a smooth user experience."
+              bgColor="bg-white text-black"
+              roundedClass="rounded-bl"
+              borderRadius={30}
+              size={250}
+            />
+            <FeatureBox
+              iconSrc={Frame4}
+              title="Collaborative Community"
+              description="Share your work and gain valuable feedback to grow as an artist or creator."
+              bgColor="bg-white text-black"
+              roundedClass="rounded-br"
+              borderRadius={30}
+              size={250}
+            />
+          </div>
+        </div>
+      </section>
 
-<Reviews />
+      <Reviews reviewsRef={reviewsRef} />
     </div>
   );
 };
@@ -230,9 +245,9 @@ const FeatureBox = ({ iconSrc, title, description, bgColor, roundedClass, border
 };
 
 // Reviews Component
-const Reviews = () => {
+const Reviews = ({ reviewsRef }) => {
   return (
-    <section className="bg-[#5282ED] text-white py-16 px-10 w-screen">
+    <section ref={reviewsRef} className="bg-[#5282ED] text-white py-16 px-10 w-screen">
       <div className="px-4 md:px-20">
         <h2 className="text-3xl font-bold text-center">What People Say</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-0 mt-8 px-4 md:px-12 justify-items-center">
@@ -240,14 +255,14 @@ const Reviews = () => {
             name="Glen Lynam"
             review="This app is awesome! It's like having a treasure map for all the coolest assets out there. I love how easy it is to find unique stuff and keep track of my favorite finds. Whether I'm browsing or buying, it makes everything super fun and simple. Can't imagine my shopping without it!"
             rating={5}
-            profilePic="Photo 1.png"
+            profilePic={Photo1}
             customMargin="md:mr-[-1px]"
           />
           <ReviewCard
             name="Olivia Maria"
             review="This app is a total game-changer! It’s like a playground for asset hunters. I can easily discover amazing deals and track my collections all in one place. It’s made shopping so much easier and way more enjoyable. Seriously, I can’t stop using it!"
             rating={5}
-            profilePic="Photo 2.png"
+            profilePic={Photo2}
             customMargin="md:ml-[-1px]"
           />
         </div>
